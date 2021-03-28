ecb3033dc4ecbb0edf908532fb3d69987ce4c36e
airyng <airyng@yandex.ru>
2021-03-29 00:56:27 +0300

    deploy fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ const envObject = require('dotenv').config()
 
 export default {
   server: {
-    port: process.env.PORT || 3000 // default: 3000
+    // port: process.env.PORT || 3000 // default: 3000
   // host: '0.0.0.0' // default: localhost
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
