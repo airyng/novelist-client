545069c4d2f9e1de3808d0f9f26c8d37e8860768
airyng <airyng@yandex.ru>
2023-02-25 23:41:36 +0300

    Added throttle function to utils plugin

--- plugins/utils.js
+++ plugins/utils.js
@@ -37,6 +37,34 @@ const getGenderTranslation = (gender) => {
   return genders[gender]
 }
 
+const throttle = function (func, ms) {
+  let isThrottled = false
+  let savedArgs
+  let savedThis
+
+  function wrapper () {
+    if (isThrottled) { // (2)
+      savedArgs = arguments
+      savedThis = this
+      return
+    }
+
+    func.apply(this, arguments) // (1)
+
+    isThrottled = true
+
+    setTimeout(function () {
+      isThrottled = false // (3)
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs)
+        savedArgs = savedThis = null
+      }
+    }, ms)
+  }
+
+  return wrapper
+}
+
 const debounce = (func, wait, immediate) => {
   // 'private' variable for instance
   // The returned function will be able to reference this due to closure.
@@ -79,5 +107,6 @@ export {
   sleep,
   getGenderTranslation,
   getGameBannerFromScenes,
+  throttle,
   debounce
 }
